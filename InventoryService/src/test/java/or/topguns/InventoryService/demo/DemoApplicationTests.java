702946c339f75199b9aad102ae311c2eba3d1e92
Siham Chatir <siham.chatir@atos.net>
2023-09-20 11:39:18 +0100

    initial1 commit

---
+++ InventoryService/src/test/java/or/topguns/InventoryService/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package or.topguns.InventoryService.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ OrderService/src/test/java/or/topguns/InventoryService/OrderService/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package or.topguns.InventoryService.OrderService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
